d30ea70e795f687aa57bed7dd2790420125bb964
unknown <dananad@126.com>
2017-03-27 20:59:44 +0800

    open zhilian fail

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,55 +1,47 @@
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainClass {
-
-    public static final HashMap<String, String> firstHashMap = new HashMap<>();
 
     public static String SAVE_DIR = "G:/page/";
 
     public static void main(String[] args) throws InterruptedException {
-//        String CHROME_DRIVER_PATH = "G:\\woshou\\chromedriver_win32\\chromedriver.exe";
-//        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
-//        String FIREFOX_DRIVER_PATH = "G:\\woshou\\geckodriver-v0.15.0-win64\\geckodriver.exe";
-//        System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setJavascriptEnabled(true);
-        caps.setCapability("takesScreenshot", true);
-        String PHANTOM_PATH = "G:\\woshou\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe";
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, PHANTOM_PATH);
-
-        WebDriver driver = new PhantomJSDriver(caps);
-//        String url = "https://passport.zhaopin.com/org/login";
-        String url = "www.baidu.com";
+        try {
+            configure();
+            prepareDriver();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        WebDriver driver = getDriver();
+        String url = "https://passport.zhaopin.com/org/login";
 
         driver.get(url);
-        Thread.sleep(10000);
+        System.out.println(driver.getPageSource());
+
         try {
             saveScreenShot((TakesScreenshot) driver, SAVE_DIR + "PhantomScreenShot.jpg");
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("CheckCodeCapt")));
-//        WebElement captchaButton = driver.findElement(By.cssSelector("#CheckCodeCapt"));
-//
-//        captchaButton.click();
-//        Thread.sleep(2000);
-//        WebElement captchaElement2 = driver.findElement(By.cssSelector("div.captcha-group"));
-//        getCaptcha((TakesScreenshot) driver, captchaElement2, SAVE_DIR + "captcha2.png");
     }
 
     private static BufferedImage getCaptcha(TakesScreenshot driver, WebElement captchaElement, String savePath) {
@@ -76,4 +68,63 @@ public class MainClass {
         FileUtils.copyFile(loginScreenshot, outFile);
     }
 
+    private static WebDriver mDriver = null;
+    private boolean mAutoQuitDriver = true;
+
+    private static final String DRIVER_FIREFOX = "firefox";
+    private static final String DRIVER_CHROME = "chrome";
+    private static final String DRIVER_PHANTOMJS = "phantomjs";
+
+    protected static DesiredCapabilities sCaps;
+
+    public static void configure() throws IOException {
+        sCaps = new DesiredCapabilities();
+        sCaps.setJavascriptEnabled(true);
+        sCaps.setCapability("takesScreenshot", true);
+
+        String driver = DRIVER_PHANTOMJS;
+
+        if (driver.equals(DRIVER_PHANTOMJS)) {
+            sCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "G:/woshou/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+            System.out.println("Test will use PhantomJS internal GhostDriver");
+        }
+
+        ArrayList<String> cliArgsCap = new ArrayList<String>();
+        cliArgsCap.add("--web-security=false");
+        cliArgsCap.add("--ssl-protocol=any");
+        cliArgsCap.add("--ignore-ssl-errors=true");
+        sCaps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+
+        sCaps.setCapability(PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS, new String[]{
+                "--logLevel=" + ("INFO")
+        });
+    }
+
+    public static void prepareDriver() throws Exception {
+        String driver = DRIVER_PHANTOMJS;
+        mDriver = new PhantomJSDriver(sCaps);
+    }
+
+    public static WebDriver getDriver() {
+        return mDriver;
+    }
+
+    protected void disableAutoQuitDriver() {
+        mAutoQuitDriver = false;
+    }
+
+    protected void enableAutoQuitDriver() {
+        mAutoQuitDriver = true;
+    }
+
+    protected boolean isAutoQuitDriverEnabled() {
+        return mAutoQuitDriver;
+    }
+
+    public void quitDriver() {
+        if (mAutoQuitDriver && mDriver != null) {
+            mDriver.quit();
+            mDriver = null;
+        }
+    }
 }
